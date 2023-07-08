59be24d18fc17eb635b738fc649c4de2c35789cf
Dhanush MC <dhanushmc93@gmail.com>
2023-07-08 08:46:44 +0530

    commit changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import *as dotenv from "dotenv";
 dotenv.config()
 
 
+
 const app = express();
 console.log(process.env.MONGO_URL);
 const PORT = process.env.PORT;
@@ -26,6 +27,17 @@ app.use("/movies",moviesroute)
 // app.get("/movies",function(request,response){
 //   response.send(movies)
 // })
+
+
+async function getHassedPassword(password){
+  const NO_OF_ROUNDS = 10;
+  const salt = await bcrypt.genSalt(NO_OF_ROUNDS);
+  const hashedPassword = await bcrypt.hash(password,salt);
+  console.log(salt)
+  console.log(hashedPassword);
+}
+getHassedPassword("password@123")
+
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
 
