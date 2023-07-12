9d32a58c8d55ae0c8ed718b619dc4a9d915f5e57
Dhanush MC <128697908+MCDhanush@users.noreply.github.com>
2023-07-12 15:45:04 +0530

    Update users.services.js

--- services/users.services.js
+++ services/users.services.js
@@ -1,10 +1,10 @@
 import { client } from "../index.js";
 
 
-export async function getUserByName(username){
-    return await client.db("mndata1").collection("users").findOne({username:username});
-}
-
 export async function createUser(data) {
     return await client.db("mndata1").collection("users").insertOne(data);
+}
+
+export async function getUserByName(username){
+    return await client.db("mndata1").collection("users").findOne({username:username});
 }
